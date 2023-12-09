db17414b1f231a48356d352cb48921f48722a954
Omer Benzer <omer.benzer@tum.de>
2023-12-09 19:28:31 +0100

    added dynamic constructor test cases

--- tests/constructor.cpp
+++ tests/constructor.cpp
@@ -2,34 +2,64 @@
 
 #include "bigint.hpp"
 
+#include <random>
+
 using ocb::BigInt;
 
-TEST_CASE("Default Constructor") {
+TEST_CASE("Simple Constructor") {
     BigInt i;
     CHECK_EQ(i.get_value(), "0");
     CHECK_EQ(i.get_negative(), false);
+
+    BigInt n{"e0"};
+    CHECK_EQ(n.get_value(), "0");
+    CHECK_EQ(n.get_negative(), false);
+    CHECK_EQ(n.get(), "0");
 }
+TEST_CASE("Random Constructor Generation") {
+    std::mt19937_64 rng;
+    std::uniform_int_distribution<long long> distribution;
 
-TEST_CASE("Custom Constructors") {
-{
-    BigInt i{"42"};
-    CHECK_EQ(i.get_value(), "42");
-    CHECK_EQ(i.get_negative(), false);
-    CHECK_EQ(i.get(), "42");
-}
+    for (int i{0}; i < 10; ++i) {
+        const auto num{distribution(rng)};
+        const auto str{std::to_string(num)};
 
-    BigInt i{"42"};
-    CHECK_EQ(i.get_value(), "42");
-    CHECK_EQ(i.get_negative(), false);
-    CHECK_EQ(i.get(), "42");
+        BigInt n{str};
+        CHECK_EQ(n.get_value(), std::to_string(std::llabs(num)));
+        CHECK_EQ(n.get_negative(), num < 0);
+        CHECK_EQ(n.get(), str);
+    }
 
-    BigInt j{"-100"};
-    CHECK_EQ(j.get_value(), "100");
-    CHECK_EQ(j.get_negative(), true);
-    CHECK_EQ(j.get(), "-100");
+    for (int i{0}; i < 10; ++i) {
+        const auto num{distribution(rng)};
+        const auto num_str{std::to_string(num)};
+        const auto exp{static_cast<u_int8_t>(distribution(rng))};
+        const auto str{std::to_string(num) + "e" + std::to_string((exp))};
 
-    BigInt k{"-0"};
-    CHECK_EQ(k.get_value(), "0");
-    CHECK_EQ(k.get_negative(), false);
-    CHECK_EQ(k.get(), "0");
+        BigInt n{str};
+        const std::string n_get{n.get()};
+        const std::string n_get_value{n.get_value()};
+
+        CHECK_EQ(n_get_value.substr(0, num_str.size()), num_str);
+        CHECK([&num_str, &n_get_value] () {
+            const auto& zeros{n_get_value.substr(num_str.size())};
+            for (const char c : zeros) {
+                if (c != '0') {
+                    return false;
+                }
+            }
+            return true;
+        }());
+
+        CHECK_EQ(n.get_negative(), num < 0);
+
+        if (num < 0) {
+            CHECK_EQ(n_get[0], '-');
+            CHECK_EQ(n_get.substr(1), n_get_value);
+        }
+        else {
+            CHECK_EQ(n_get.size(), n_get_value.size());
+            CHECK_EQ(n_get, n_get_value);
+        }
+    }
 }
