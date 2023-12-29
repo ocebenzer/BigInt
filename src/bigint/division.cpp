447332d2fbace8ba69c039f14db937cdb4bb83b0
Omer Benzer <omer.benzer@tum.de>
2023-12-29 15:26:38 +0100

    division implemented | division tests

--- src/bigint/division.cpp
+++ src/bigint/division.cpp
@@ -1,8 +1,85 @@
 #include "bigint.hpp"
+#include "utils.hpp"
+
+#include <sstream>
 
 namespace ocb {
 
 BigInt& BigInt::operator/= (const BigInt& other) {
+    if (other == 0) {
+        throw std::invalid_argument("divide by 0");
+    }
+
+    const auto& divisor{other.get_value()};
+    int start_index{0};
+    int end_index(1);
+
+    if (is_less_than(this->get_value(), divisor)) {
+        this->set_value("0");
+        this->set_negative(false);
+        return *this;
+    }
+
+    {
+        auto reduced_this_value{this->get_value().substr(start_index, end_index)};
+        while (is_less_than(reduced_this_value, divisor)) {
+            ++end_index;
+            reduced_this_value = this->get_value().substr(start_index, end_index);
+        }
+    }
+
+    {
+        std::string divident{this->get_value()};
+        std::stringstream quotient;
+
+        for (; end_index <= divident.size(); ++end_index) {
+            const std::string_view partial_divident{divident.begin()+start_index, divident.begin()+end_index};
+
+            if (!is_less_than(partial_divident, divisor)) {
+                std::string partial_divisor{divisor};
+                int partial_quotient{1};
+
+                while (is_less_than(partial_divisor, partial_divident)) {
+                    partial_divisor = add(partial_divisor, divisor);
+                    ++partial_quotient;
+                }
+                // if remainder, undo last addition
+                if (is_less_than(partial_divident, partial_divisor)) {
+                    partial_divisor = sub(partial_divisor, divisor);
+                    trim_zeros(partial_divisor);
+                    --partial_quotient;
+                }
+                quotient << static_cast<char>(partial_quotient + '0');
+
+                {
+                    const std::string padding(divident.size()-end_index, '0');
+                    const std::string padded_partial_divisor{partial_divisor + padding};
+
+                    if (divident == padded_partial_divisor) {
+                        quotient << padding;
+                        break;
+                    }
+
+                    divident = sub(divident, padded_partial_divisor);
+                    while (divident[start_index] == '0') {
+                        ++start_index;
+                    }
+                }
+            }
+            else {
+                quotient << static_cast<char>('0');
+            }
+        }
+
+        {
+            auto result{quotient.str()};
+            trim_zeros(result);
+
+            this->set_value(std::move(result));
+            this->set_negative(this->get_negative() ^ other.get_negative());
+        }
+    }
+
     return *this;
 };
 
--- tests/division.cpp
+++ tests/division.cpp
@@ -6,3 +6,107 @@
 #include <random>
 
 using ocb::BigInt;
+
+TEST_CASE("Simple Divison") {
+    BigInt i{15};
+    i /= 3;
+
+    CHECK_EQ(i.get(), "5");
+    CHECK_EQ(i.get_value(), "5");
+    CHECK_EQ(i.get_negative(), false);
+
+    auto res{i / 5};
+
+    CHECK_EQ(res.get(), "1");
+    CHECK_EQ(res.get_value(), "1");
+    CHECK_EQ(res.get_negative(), false);
+}
+
+TEST_CASE("Negative Divison") {
+    BigInt i{-60};
+    CHECK_EQ(i.get(), "-60");
+    CHECK_EQ(i.get_value(), "60");
+    CHECK_EQ(i.get_negative(), true);
+
+    i /= -3;
+    CHECK_EQ(i.get(), "20");
+    CHECK_EQ(i.get_value(), "20");
+    CHECK_EQ(i.get_negative(), false);
+
+    auto res{i / -5};
+    CHECK_EQ(res.get(), "-4");
+    CHECK_EQ(res.get_value(), "4");
+    CHECK_EQ(res.get_negative(), true);
+
+    res = res / -2;
+    CHECK_EQ(res.get(), "2");
+    CHECK_EQ(res.get_value(), "2");
+    CHECK_EQ(res.get_negative(), false);
+
+    res /= -2;
+    CHECK_EQ(res.get(), "-1");
+    CHECK_EQ(res.get_value(), "1");
+    CHECK_EQ(res.get_negative(), true);
+}
+
+TEST_CASE("Divison with Remainder") {
+    BigInt i{51};
+    CHECK_EQ(i.get(), "51");
+    CHECK_EQ(i.get_value(), "51");
+    CHECK_EQ(i.get_negative(), false);
+
+    i /= 10;
+    CHECK_EQ(i.get(), "5");
+    CHECK_EQ(i.get_value(), "5");
+    CHECK_EQ(i.get_negative(), false);
+
+    auto res{i / 3};
+    CHECK_EQ(res.get(), "1");
+    CHECK_EQ(res.get_value(), "1");
+    CHECK_EQ(res.get_negative(), false);
+}
+
+TEST_CASE("Negative Divison with Remainder") {
+    BigInt i{-51};
+    CHECK_EQ(i.get(), "-51");
+    CHECK_EQ(i.get_value(), "51");
+    CHECK_EQ(i.get_negative(), true);
+
+    i /= -10;
+    CHECK_EQ(i.get(), "5");
+    CHECK_EQ(i.get_value(), "5");
+    CHECK_EQ(i.get_negative(), false);
+
+    auto res{i / 3};
+    CHECK_EQ(res.get(), "1");
+    CHECK_EQ(res.get_value(), "1");
+    CHECK_EQ(res.get_negative(), false);
+}
+
+TEST_CASE("Random Generation") {
+    std::mt19937_64 rng;
+    std::uniform_int_distribution<int64_t> distribution;
+
+    for (int i{0}; i < 10; ++i) {
+        const int64_t n1{distribution(rng)};
+        const int64_t n2{static_cast<int32_t>(distribution(rng))};
+        if (n2 == 0) continue;
+        const int64_t div{n1 / n2};
+
+        BigInt big1{n1};
+        BigInt big2{n2};
+        BigInt big_div{big1 / big2};
+        CHECK_EQ(big_div, div);
+        CHECK_EQ(big_div.get_negative(), div < 0);
+        CHECK_EQ(big_div.get(), std::to_string(div));
+    }
+}
+
+TEST_CASE("Divide by Zero") {
+    BigInt i{5};
+    CHECK_EQ(i.get(), "5");
+    CHECK_EQ(i.get_value(), "5");
+    CHECK_EQ(i.get_negative(), false);
+
+    CHECK_THROWS(i /= 0);
+}
